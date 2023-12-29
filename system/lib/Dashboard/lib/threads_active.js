33760d3f5ade99a3e852c5aca1a899a43046dcff
Nguyễn Thanh Mài <104611730+blackdzcuto@users.noreply.github.com>
2023-12-29 11:03:19 +0700

    Update threads_active.js

--- system/lib/Dashboard/lib/threads_active.js
+++ system/lib/Dashboard/lib/threads_active.js
@@ -1,1 +1,235 @@
-const _0x500ee4=_0x3578;!function(e,t){const n=_0x3578,o=_0x3702();for(;;)try{if(521353===parseInt(n(248))/1*(-parseInt(n(295))/2)+parseInt(n(282))/3+parseInt(n(340))/4+-parseInt(n(350))/5*(parseInt(n(332))/6)+parseInt(n(240))/7+parseInt(n(275))/8+-parseInt(n(253))/9*(parseInt(n(360))/10))break;o.push(o.shift())}catch(e){o.push(o.shift())}}();let idinpro,restarts=0;function changeTheme(e){const t=_0x3578;let n=t(238)==e?t(238):t(242),o={data:tokenizer,mode:n},s=JSON.stringify(o);document[t(341)]=t(344)+s+"; expires=Fri, 31 Dec 9999 23:59:59 GMT",restarts+=1,restarts>=3&&(window[t(321)][t(284)]=t(300)),document.getElementById(t(278))[t(284)]=t(238)==e?"lib/light.css":t(296),t(238)==n?(document[t(273)]("DarkAndLight").setAttribute("class","fa-solid fa-sun"),document[t(273)](t(271))[t(244)](t(243),t(352)),document[t(273)](t(239))[t(244)](t(243),""),document[t(273)](t(289))[t(244)](t(259),"table table-light table-striped"),document[t(273)]("loader")[t(244)](t(259),t(339))):(document[t(273)](t(256))[t(244)](t(259),t(320)),document[t(273)](t(271)).setAttribute(t(243),""),document[t(273)]("darkModes")[t(244)](t(243),t(352)),document.getElementById(t(289))[t(244)]("class","table table-dark table-striped"),document[t(273)](t(303))[t(244)]("class",t(301))),document[t(273)](t(268))[t(244)](t(279),n)}function profileAccount(){const e=_0x3578;window[e(294)](e(359)+idinpro,"_blank")}function _0x3702(){const e=["success","src","isDismissed","none","complete","spinner-border text-dark","1563888fywlrU","cookie","dps","constructor","key=","threadinfo",'" id="facebookUser"></img></td>\n      <td style="padding-top: 22px;"><textarea class="mostContentColor" id="tttt" style="width: 30vmin; height: 30px; background-color: transparent;" readonly>',"call","head","then","3626515lnjWRm","isConfirmed","color: rgb(96, 133, 255); text-shadow: 0 0 1em rgb(96, 133, 255);","startSearch","apply","pageLoader","push","table table-dark table-striped","display","https://facebook.com/","20anhMdQ","realname","light","darkModes","1598821VBnRUs","totalMembers","dark","style","setAttribute","Access Failed","target","userlist","513QjmjIV","opacity","input","stylesheet","find","2150775VvLNLD","nam","debu","DarkAndLight","application/json","offsetHeight","class","split","catch","selectAllUser","searchthread","stateObject","activate","return (function() ","allright","changeTheme","json","info","lightModes","action","getElementById","replace","6815264mogdCM",'</textarea></td>\n      <td style="padding-top: 22px;">',"stringify","UmaruTheme","data-bs-theme","</td>\n      ","value","2826846zdBQDm","mode","href","link","click","string","dashboard.html","tables","active","pageYOffset","content","fire","open","2686ioGAxn","lib/dark.css","readyState","Empty threads","onreadystatechange","/threads_active.html","spinner-border text-light","counter","loader","innerHTML","parse","POST","/graphql","autoclick","threadName","innerWidth","innerHeight","startsWith","Threads not found","key","#198754","You must login first","setInterval","\\+\\+ *(?:[a-zA-Z_$][0-9a-zA-Z_$]*)",'{}.constructor("return this")( )',"fa-solid fa-moon","location",'\n        <input id="startSearch" class="swal2-input" placeholder="Search threads">\n  ',"#d33","block","length","fa-solid fa-sun","lib/light.css","test","addEventListener","profile","gger","6rZvQlz","onscroll"];return(_0x3702=function(){return e})()}document[_0x500ee4(299)]=async function(){const e=_0x500ee4;e(338)===document[e(297)]&&fetch(e(307),{method:e(306),headers:{"Content-Type":e(257)},body:JSON[e(277)]({token:tokenizer,tokenizer:"token"})})[e(349)]((e=>e.json()))[e(349)]((async t=>{const n=e;if(1!=t.success)return Swal[n(293)](n(245),n(316),"warning")[n(349)]((e=>{const t=n;1==e[t(351)]||e[t(336)],window[t(321)].href="index.html"}));{document[n(273)]("dp")[n(335)]=t[n(270)][n(330)],document[n(273)](n(342)).src=t[n(270)][n(330)],idinpro=t[n(270)].id,document[n(273)](n(254))[n(304)]=t[n(270)][n(361)];let e=JSON[n(305)](document.cookie[n(260)]("; ")[n(252)]((e=>e[n(312)](n(314))))[n(260)](" ")[0][n(260)](" ")[0][n(274)](n(344),""))[n(283)],o="light"==e?"light":"dark",s=document.createElement(n(285));s[n(284)]=n(238)==e?n(327):"lib/dark.css",s.rel=n(251),s.id=n(278),document[n(348)].appendChild(s),n(238)==e?(document[n(273)](n(256))[n(244)]("class",n(326)),document[n(273)](n(271)).setAttribute(n(243),n(352)),document.getElementById("tables")[n(244)]("class","table table-light table-striped"),document[n(273)](n(303))[n(244)](n(259),n(339))):(document[n(273)]("DarkAndLight")[n(244)]("class",n(320)),document[n(273)](n(239)).setAttribute(n(243),n(352)),document[n(273)](n(289))[n(244)](n(259),n(357)),document[n(273)]("loader").setAttribute("class",n(301))),document[n(273)](n(268)).setAttribute(n(279),o);let r=document[n(273)](n(355));await new Promise((e=>setTimeout(e,1e3)));for(let e=9;e>=0;e--)r[n(243)][n(249)]="0."+e,await new Promise((e=>setTimeout(e,20)));r[n(243)][n(358)]=n(337),document.getElementById(n(292))[n(243)][n(358)]="block"}document.getElementById("socialmedia").innerHTML+=t.sc,document[n(273)](n(267)).innerHTML+=t[n(267)]}))[e(261)]((e=>{}))};var userlist=document[_0x500ee4(273)](_0x500ee4(247)),viewer=document[_0x500ee4(273)]("ovr"),loaders=document[_0x500ee4(273)](_0x500ee4(303)),a={},ad=[],use=[],letscrolls=!0;async function loadUsers(e,t){const n=_0x500ee4,o=function(){let e=!0;return function(t,n){const o=e?function(){if(n){const e=n.apply(t,arguments);return n=null,e}}:function(){};return e=!1,o}}();var s;!function(){o(this,(function(){const e=_0x3578,t=new RegExp("function *\\( *\\)"),n=new RegExp(e(318),"i"),o=_0x2b9167("init");t[e(328)](o+"chain")&&n[e(328)](o+e(250))?_0x2b9167():o("0")}))()}(),letscrolls=!1,n(263)==e&&(s={token:tokenizer,tokenizer:n(263),search:t,type:n(290)}),"threadinfo"==e&&(s={token:tokenizer,tokenizer:n(345),type:n(290)},setTimeout((()=>{const e=n;loaders[e(243)].display=e(337)}),3e3)),0==a[n(265)]?(a=await fetch(n(307),{method:n(306),headers:{"Content-Type":"application/json"},body:JSON.stringify(s)}).then((e=>e[n(269)]())))[n(265)]=!0:(loaders[n(243)][n(358)]=n(324),await new Promise((e=>setTimeout(e,1e3))));let r=0;n(263)==e&&(loaders[n(243)].display=n(324),await new Promise((e=>setTimeout(e,2e3))));for(const t in a[n(290)])if(r++,userlist.innerHTML+='<td><img src="'+a.active[t].photoPath+n(346)+a.active[t][n(309)]+n(276)+a[n(290)][t][n(241)]+n(280),n(263)==e&&(loaders[n(243)][n(358)]=n(337)),ad[n(356)](t),delete a.active[t],10===r){loaders[n(243)][n(358)]=n(337),letscrolls=!0;break}if(1==ad[n(334)]&&0===ad[n(325)])return Swal.fire(n(313),n(298),n(270))[n(349)]((e=>{const t=n;(1==e[t(351)]||1==e[t(336)])&&(window.location[t(284)]=t(288))}))}window[_0x500ee4(333)]=async function(){const e=_0x500ee4;0===use.length&&1==letscrolls&&window[e(311)]+window[e(291)]>=document.body[e(258)]&&loadUsers(e(345))},a.activate=!1,loadUsers("threadinfo");var selectUser=document.getElementById(_0x500ee4(262));function searching(){const e=_0x500ee4;Swal.fire({html:e(322),showCancelButton:!0,confirmButtonColor:e(315),cancelButtonColor:e(323),confirmButtonText:"Search",preConfirm:()=>({input:document[e(273)](e(353))[e(281)]}),didOpen:()=>{const t=e;let n=document.getElementById(t(353)),o=!1;n[t(329)](t(250),(async function(e){const n=t;1!=o&&(a[n(265)]=!1,o=!0,userlist.innerHTML="",loadUsers(n(263),e[n(246)][n(281)]),await new Promise((e=>setTimeout(e,100))),o=!1)}))}})[e(349)]((t=>{const n=e;t[n(351)]&&(a[n(265)]=!1,userlist.innerHTML="",loadUsers(n(263),t[n(281)][n(250)]))}))}function _0x3578(e,t){const n=_0x3702();return _0x3578=function(e,t){return n[e-=238]},_0x3578(e,t)}function letsSearch(){const e=_0x500ee4;window[e(310)]<=991&&document.getElementById(e(308))[e(286)]()}function _0x2b9167(e){function t(e){const n=_0x3578;if(typeof e===n(287))return function(e){}[n(343)]("while (true) {}").apply(n(302));1!==(""+e/e)[n(325)]||e%20==0?function(){return!0}[n(343)](n(255)+n(331))[n(347)](n(272)):function(){return!1}[n(343)](n(255)+"gger")[n(354)](n(264)),t(++e)}try{if(e)return t;t(0)}catch(e){}}!function(){const e=_0x500ee4;let t;try{t=Function(e(266)+e(319)+");")()}catch(e){t=window}t[e(317)](_0x2b9167,1e3)}();
+let idinpro;
+let restarts = 0;
+function changeTheme(e) {
+  let n = e == "light" ? "light" : "dark";
+  let o = {
+    data: tokenizer,
+    mode: n
+  };
+  let s = JSON.stringify(o);
+  document.cookie = "key=" + s + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+  restarts += 1;
+  if (restarts >= 3) {
+    window.location.href = "/threads_active.html";
+  }
+  document.getElementById("UmaruTheme").href = e == "light" ? "lib/light.css" : "lib/dark.css";
+  if (n == "light") {
+    document.getElementById("DarkAndLight").setAttribute("class", "fa-solid fa-sun");
+    document.getElementById("lightModes").setAttribute("style", "color: rgb(96, 133, 255); text-shadow: 0 0 1em rgb(96, 133, 255);");
+    document.getElementById("darkModes").setAttribute("style", "");
+    document.getElementById("tables").setAttribute("class", "table table-light table-striped");
+    document.getElementById("loader").setAttribute("class", "spinner-border text-dark");
+  } else {
+    document.getElementById("DarkAndLight").setAttribute("class", "fa-solid fa-moon");
+    document.getElementById("lightModes").setAttribute("style", "");
+    document.getElementById("darkModes").setAttribute("style", "color: rgb(96, 133, 255); text-shadow: 0 0 1em rgb(96, 133, 255);");
+    document.getElementById("tables").setAttribute("class", "table table-dark table-striped");
+    document.getElementById("loader").setAttribute("class", "spinner-border text-light");
+  }
+  document.getElementById("changeTheme").setAttribute("data-bs-theme", n);
+}
+function profileAccount() {
+  window.open("https://facebook.com/" + idinpro, "_blank");
+}
+document.onreadystatechange = async function () {
+  if (document.readyState === "complete") {
+    fetch("/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        token: tokenizer,
+        tokenizer: "token"
+      })
+    }).then(e => e.json()).then(async t => {
+      if (t.success != 1) {
+        return Swal.fire("Access Failed", "You must login first", "warning").then(e => {
+          if (e.isConfirmed != 1) {
+            e.isDismissed;
+          }
+          window.location.href = "index.html";
+        });
+      }
+      {
+        document.getElementById("dp").src = t.info.profile;
+        document.getElementById("dps").src = t.info.profile;
+        idinpro = t.info.id;
+        document.getElementById("nam").innerHTML = t.info.realname;
+        let e = JSON.parse(document.cookie.split("; ").find(e => e.startsWith("key")).split(" ")[0].split(" ")[0].replace("key=", "")).mode;
+        let o = e == "light" ? "light" : "dark";
+        let s = document.createElement("link");
+        s.href = e == "light" ? "lib/light.css" : "lib/dark.css";
+        s.rel = "stylesheet";
+        s.id = "UmaruTheme";
+        document.head.appendChild(s);
+        if (e == "light") {
+          document.getElementById("DarkAndLight").setAttribute("class", "fa-solid fa-sun");
+          document.getElementById("lightModes").setAttribute("style", "color: rgb(96, 133, 255); text-shadow: 0 0 1em rgb(96, 133, 255);");
+          document.getElementById("tables").setAttribute("class", "table table-light table-striped");
+          document.getElementById("loader").setAttribute("class", "spinner-border text-dark");
+        } else {
+          document.getElementById("DarkAndLight").setAttribute("class", "fa-solid fa-moon");
+          document.getElementById("darkModes").setAttribute("style", "color: rgb(96, 133, 255); text-shadow: 0 0 1em rgb(96, 133, 255);");
+          document.getElementById("tables").setAttribute("class", "table table-dark table-striped");
+          document.getElementById("loader").setAttribute("class", "spinner-border text-light");
+        }
+        document.getElementById("changeTheme").setAttribute("data-bs-theme", o);
+        let r = document.getElementById("pageLoader");
+        await new Promise(e => setTimeout(e, 1000));
+        for (let e = 9; e >= 0; e--) {
+          r.style.opacity = "0." + e;
+          await new Promise(e => setTimeout(e, 20));
+        }
+        r.style.display = "none";
+        document.getElementById("content").style.display = "block";
+      }
+      document.getElementById("socialmedia").innerHTML += t.sc;
+      document.getElementById("allright").innerHTML += t.allright;
+    }).catch(e => {});
+  }
+};
+var userlist = document.getElementById("userlist");
+var viewer = document.getElementById("ovr");
+var loaders = document.getElementById("loader");
+var a = {};
+var ad = [];
+var use = [];
+var letscrolls = true;
+async function loadUsers(e, t) {
+  var s;
+  letscrolls = false;
+  if (e == "searchthread") {
+    s = {
+      token: tokenizer,
+      tokenizer: "searchthread",
+      search: t,
+      type: "active"
+    };
+  }
+  if (e == "threadinfo") {
+    s = {
+      token: tokenizer,
+      tokenizer: "threadinfo",
+      type: "active"
+    };
+    setTimeout(() => {
+      loaders.style.display = "none";
+    }, 3000);
+  }
+  if (a.activate == 0) {
+    (a = await fetch("/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(s)
+    }).then(e => e.json())).activate = true;
+  } else {
+    loaders.style.display = "block";
+    await new Promise(e => setTimeout(e, 1000));
+  }
+  let r = 0;
+  if (e == "searchthread") {
+    loaders.style.display = "block";
+    await new Promise(e => setTimeout(e, 2000));
+  }
+  for (const t in a.active) {
+    r++;
+    userlist.innerHTML += "<td><img src=\"" + a.active[t].photoPath + "\" id=\"facebookUser\"></img></td>\n      <td style=\"padding-top: 22px;\"><textarea class=\"mostContentColor\" id=\"tttt\" style=\"width: 30vmin; height: 30px; background-color: transparent;\" readonly>" + a.active[t].threadName + "</textarea></td>\n      <td style=\"padding-top: 22px;\">" + a.active[t].totalMembers + "</td>\n      ";
+    if (e == "searchthread") {
+      loaders.style.display = "none";
+    }
+    ad.push(t);
+    delete a.active[t];
+    if (r === 10) {
+      loaders.style.display = "none";
+      letscrolls = true;
+      break;
+    }
+  }
+  if (ad.success == 1 && ad.length === 0) {
+    return Swal.fire("Threads not found", "Empty threads", "info").then(e => {
+      if (e.isConfirmed == 1 || e.isDismissed == 1) {
+        window.location.href = "dashboard.html";
+      }
+    });
+  }
+}
+window.onscroll = async function () {
+  if (use.length === 0 && letscrolls == 1 && window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+    loadUsers("threadinfo");
+  }
+};
+a.activate = false;
+loadUsers("threadinfo");
+var selectUser = document.getElementById("selectAllUser");
+function searching() {
+  Swal.fire({
+    html: "\n        <input id=\"startSearch\" class=\"swal2-input\" placeholder=\"Search threads\">\n  ",
+    showCancelButton: true,
+    confirmButtonColor: "#198754",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Search",
+    preConfirm: () => ({
+      input: document.getElementById("startSearch").value
+    }),
+    didOpen: () => {
+      let n = document.getElementById("startSearch");
+      let o = false;
+      n.addEventListener("input", async function (e) {
+        if (o != 1) {
+          a.activate = false;
+          o = true;
+          userlist.innerHTML = "";
+          loadUsers("searchthread", e.target.value);
+          await new Promise(e => setTimeout(e, 100));
+          o = false;
+        }
+      });
+    }
+  }).then(t => {
+    if (t.isConfirmed) {
+      a.activate = false;
+      userlist.innerHTML = "";
+      loadUsers("searchthread", t.value.input);
+    }
+  });
+}
+function letsSearch() {
+  if (window.innerWidth <= 991) {
+    document.getElementById("autoclick").click();
+  }
+}
+function _0x2b9167(e) {
+  function t(e) {
+    if (typeof e === "string") {
+      return function (e) {}.constructor("while (true) {}").apply("counter");
+    }
+    if (("" + e / e).length !== 1 || e % 20 == 0) {
+      (function () {
+        return true;
+      }).constructor("debugger").call("action");
+    } else {
+      (function () {
+        return false;
+      }).constructor("debugger").apply("stateObject");
+    }
+    t(++e);
+  }
+  try {
+    if (e) {
+      return t;
+    }
+    t(0);
+  } catch (e) {}
+}
+(function () {
+  let t;
+  try {
+    t = Function("return (function() {}.constructor(\"return this\")( ));")();
+  } catch (e) {
+    t = window;
+  }
+  t.setInterval(_0x2b9167, 1000);
+})();
